2579950baacab6dcaf2b74f38f5c75bca5f9ff1f
IzayoiMiku <tx137884746@gmail.com>
2016-06-10 23:08:52 +0800

    Update forms.py

--- toughradius/console/customer/forms.py
+++ toughradius/console/customer/forms.py
@@ -6,10 +6,10 @@ from toughradius.console.libs.pyforms.rules import button_style,input_style
 
 boolean = {0:u"否", 1:u"是"}
 
-sexopt = {1:u"男",0:u"女"}
+sexopt = {1:u"♂",0:u"♀"}
 
 member_login_form = pyforms.Form(
-    pyforms.Textbox("username", rules.len_of(1, 32), description=u"用户名", size=32,required="required",**input_style),
+    pyforms.Textbox("username", rules.len_of(1, 32), description=u"用户账号", size=32,required="required",**input_style),
     pyforms.Password("password", rules.len_of(1,32), description=u"登录密码", size=32, required="required",**input_style),
     pyforms.Button("submit", type="submit", html=u"<b>登陆</b>", **button_style),
     pyforms.Hidden("next",value="/"),
@@ -20,15 +20,15 @@ member_login_form = pyforms.Form(
 def member_join_form(nodes=[]): 
     return pyforms.Form(
         pyforms.Dropdown("node_id", description=u"区域", args=nodes,required="required", **input_style),
-        pyforms.Textbox("realname", rules.len_of(2,32), description=u"用户姓名(必填)", required="required",**input_style),
-        pyforms.Dropdown("sex", description=u"性别", args=sexopt.items(),required="required", **input_style),
+        pyforms.Textbox("realname", rules.len_of(2,32), description=u"用户ID(必填)", required="required",**input_style),
+        pyforms.Dropdown("sex", description=u"♂&♀", args=sexopt.items(),required="required", **input_style),
         pyforms.Textbox("age", rules.is_number, description=u"年龄(必填)", size=3,required="required",**input_style),
-        pyforms.Textbox("username", rules.is_alphanum3(6, 32), description=u"用户名(必填)", size=32,required="required",**input_style),
+        pyforms.Textbox("username", rules.is_alphanum3(6, 32), description=u"登录账号(必填)", size=32,required="required",**input_style),
         pyforms.Password("password", rules.len_of(6,32), description=u"登录密码(必填)", size=32, required="required",**input_style),
         pyforms.Textbox("email", rules.is_email, description=u"电子邮箱(必填)", size=64,required="required",**input_style),
-        pyforms.Textbox("idcard", rules.len_of(0,32), description=u"证件号码", **input_style),
-        pyforms.Textbox("mobile", rules.len_of(0,32),description=u"用户手机号码", **input_style),
-        pyforms.Textbox("address", description=u"用户地址",hr=True, **input_style),
+        pyforms.Textbox("idcard", rules.len_of(0,32), description=u"序列码", **input_style),
+        pyforms.Textbox("mobile", rules.len_of(0,32),description=u"QQ", **input_style),
+        pyforms.Textbox("address", description=u"二次校验码",hr=True, **input_style),
         pyforms.Button("submit", type="submit", html=u"<b>注册</b>", **button_style),
         action="/join",
         title=u"用户注册"
@@ -45,7 +45,7 @@ password_update_form =  pyforms.Form(
     )
 
 password_mail_form =  pyforms.Form(
-        pyforms.Textbox("member_name", rules.len_of(1, 64),description=u"请输入登录名", required="required",**input_style),
+        pyforms.Textbox("member_name", rules.len_of(1, 64),description=u"请输入账号", required="required",**input_style),
         pyforms.Button("submit",  type="submit", html=u"<b>提交</b>", **button_style),
         title=u"重置密码请求",
         action="/password/mail"
